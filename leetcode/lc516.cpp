704731b050fd4f9c7b16d02e1587e9fe3223e24b
pbsureshbabu <p.b.sureshbabu@gmail.com>
2018-05-18 12:38:35 -0700

    Leetcode prob #516 - longest palindromic subsequence

--- leetcode/lc516.cpp
+++ leetcode/lc516.cpp
@@ -7,36 +7,31 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Solution {
-private:
-	int lut[1000][1000];
 public:
 	int max(int a, int b) {return a>b?a:b;}
-	int L( string s, int lut[1000][1000], int i, int  j )
-	{
-		if(i>j) 
-			return 0;
-		if(lut[i][j]!=-1) 
-			return lut[i][j];
-		if(i==j) 
-			{ lut[i][j]=1; return lut[i][j];}
-		if(s[i]==s[j]) 
-			{ lut[i][j]=2+L(s, lut, i+1, j-1); return lut[i][j];}
-		else 
-			{ lut[i][j]=max(L(s, lut, i+1, j), L(s, lut, i, j-1)); return lut[i][j];}
-	}
-	
     int longestPalindromeSubseq(string s) {
-		//vector<vector<int> > lut(s.size(), vector<int> (s.size()));
-		if(s.size()==0)
-			return 0;
-
-		for(int i=0; i<s.size(); i++)
-			for(int j=0; j<s.size(); j++)
-				lut[i][j]=-1;
-        return L(s, lut, 0, s.size()-1);
+		vector<int> prev(s.length()+1, 0);
+		vector<int> cur(s.length()+1, 0);
+		string p=s;
+		reverse(p.begin(), p.end());
+		cout << p << endl;
+		for(int i=1; i<=s.length(); i++)
+		{
+			for(int j=1; j<=s.length(); j++)
+				if(s[i-1]==p[j-1])
+					cur[j]=prev[j-1]+1;
+				else
+					cur[j]=max(cur[j-1], prev[j]);
+			//for(int k=0; k<cur.size(); k++)
+			//	cout << cur[k] << " ";
+			//cout << endl;
+			prev=cur;
+		}
+		return prev[s.length()];
     }
 };
 
@@ -46,7 +41,7 @@ int main()
 	Solution sol;
 	int res=0;
 	string s="hiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxrhiddqscdxr";
-
+	//string s="suresh";
 	res = sol.longestPalindromeSubseq(s);
 
 	cout << res << endl;

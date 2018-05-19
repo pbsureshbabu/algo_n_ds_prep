dcd0f27236864f0b17e0a77c057c5eb82051911c
pbsureshbabu <p.b.sureshbabu@gmail.com>
2018-05-19 10:55:09 -0700

    #1 done in graph techie delight.

--- techie_delight/1.cpp
+++ techie_delight/1.cpp
@@ -6,23 +6,44 @@ using namespace std;
 
 #define MAXV    (20)
 
+// this is for just passing the data to the graph creation
 typedef struct edge_
 {
-	int src, dst;
+	int src, dst, wt;
 }edges_t;
 
+// adjacency list node
 typedef struct adjNodes_
 {
 	int dst;
+	int wt;
 	struct adjNodes_ *next;
 } adjNodes_t;
 
+// graph struct
 typedef struct graph_
 {
 	adjNodes_t *adjNodes[MAXV];
 } graph_t;
 
-graph_t *create_graph( edges_t *edges, int tot_edges )
+// check whether the edge is already in the adjacency list
+bool is_in_the_list( graph_t *g, int src, int dst )
+{
+	if(src>=MAXV || dst>=MAXV)
+		return true;
+	adjNodes_t *temp;
+	temp=g->adjNodes[src];
+	while(temp)
+	{
+		if(temp->dst==dst)
+			return true;
+		temp=temp->next;
+	}
+	return false;
+}
+
+// create a graph
+graph_t *create_graph( edges_t *edges, int tot_edges, bool is_directed )
 {
 	graph_t *g;
 	g = (graph_t *)malloc(sizeof(graph_t));
@@ -34,16 +55,38 @@ graph_t *create_graph( edges_t *edges, int tot_edges )
 	{
 		int src=edges[i].src;
 		int dst=edges[i].dst;
+		int wt=edges[i].wt;
 		if(!(src<MAXV && dst<MAXV))
 			continue;
-		adjNodes_t *anode = (adjNodes_t *)malloc(sizeof(adjNodes_t));
-		anode->dst = dst;
-		anode->next = g->adjNodes[src];
-		g->adjNodes[src]=anode;
+		
+		// creating the edge from source to destination
+		if(!is_in_the_list(g, src, dst))
+		{
+			adjNodes_t *anode = (adjNodes_t *)malloc(sizeof(adjNodes_t));
+			anode->dst = dst;
+			anode->wt = wt;
+			anode->next = g->adjNodes[src];
+			g->adjNodes[src]=anode;
+		}
+
+		if(!is_directed)
+		{
+			// also create the edge from dest to src
+			// it would be nice to check, if for that particular dst, where there is a particular src
+			if(!is_in_the_list(g, dst, src))
+			{
+				adjNodes_t *anode = (adjNodes_t *)malloc(sizeof(adjNodes_t));
+				anode->dst = src;
+				anode->wt = wt;
+				anode->next = g->adjNodes[dst];
+				g->adjNodes[dst]=anode;
+			}
+		}
 	}
 	return g;
 }
 
+// delete the graph
 void delete_graph( graph_t *g)
 {
 	if(!g)
@@ -63,6 +106,7 @@ void delete_graph( graph_t *g)
 	free(g);
 }
 
+// print the graph
 void print_graph( graph_t *g )
 {
 	for(int i=0; i<MAXV; i++)
@@ -73,7 +117,7 @@ void print_graph( graph_t *g )
 			continue;
 		while(temp)
 		{
-			cout << "(" << i << "," << temp->dst << ") ";
+			cout << "[" << i << "," << temp->dst << "(" << temp->wt << ")]  " ;
 			temp=temp->next;
 		}
 		cout << endl;
@@ -82,10 +126,10 @@ void print_graph( graph_t *g )
 
 int main()
 {
-	edges_t edges[] = {{0,1}, {1,2}, {2,0}, {2,1}, {3,2}, {4,5}, {5,4}};
+	edges_t edges[] = {{0,1,6}, {1,2,7}, {2,0,5}, {2,1,4}, {3,2,10}, {4,5,1}, {5,4,3}};
 	int tot_edges=sizeof(edges)/sizeof(edges[0]);
 
-	graph_t *g=create_graph( edges, tot_edges );
+	graph_t *g=create_graph( edges, tot_edges, true );
 	if(!g)
 		return 0;
 	print_graph(g);

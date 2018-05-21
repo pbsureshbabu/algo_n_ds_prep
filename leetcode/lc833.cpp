b883e83d76fcf73d655fb6c7a6a3097785008a6c
pbsureshbabu <p.b.sureshbabu@gmail.com>
2018-05-21 07:34:07 -0700

    none.

---
+++ leetcode/lc833.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string findReplaceString(string S, vector<int>& indexes, 
+		vector<string>& sources, vector<string>& targets) 
+	{
+        
+    }
+};
+
+
+int main()
+{
+	return 0;
+}
